9b37e5e978b8ae23b10d6959198f56df2882e52d
Abdelrahman_Elnahrawy <abd0elna7rawy@gmail.com>
2024-10-11 02:02:26 +0300

    first commit message

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "avr8-stub.h"
+#define LED PIN_PB7
+#define BUTTON PIN_PE0
+
+
+void setup() {
+  pinMode(LED,OUTPUT);
+  pinMode(BUTTON,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(digitalRead(BUTTON)==0){
+  digitalWrite(LED,HIGH);
+  delay(100);
+  }
+  else{
+  digitalWrite(LED,LOW);
+  }
+  
+}
+
